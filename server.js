d19f9e9a84d9e5e64e92cdbd97c659b7c114d085
KobeHill68 <kobehill68@gmail.com>
2021-01-18 10:08:31 -0600

    still working on the server.js file

--- server.js
+++ server.js
@@ -9,3 +9,19 @@ app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+    process.env.MONGOB_URI || '',
+    {
+
+    }
+);
+
+app.use(require('./routes/html-routes'));
+app.use(require('./routes/api-routes'));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+})
