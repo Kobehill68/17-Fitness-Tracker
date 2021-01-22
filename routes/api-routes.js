8c942a083bcfe01f6338890f3e2208fc1fc90ff7
KobeHill68 <kobehill68@gmail.com>
2021-01-22 10:11:14 -0600

    done with this route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,5 @@
-const router = require("express").Router();
+const router = require("express").Router()
 const db = require("../models");
-
 
 router.get("/api/workouts", (req, res) => {
     console.log("All Workouts");
@@ -10,30 +9,62 @@ router.get("/api/workouts", (req, res) => {
                 totalDuration: { $sum: "$exercises.duration" }
             }
         }
-
     ])
         .then(dbWorkout => {
-            res.json(dbWorkout)
+            res.json(dbWorkout);
         })
         .catch(err => {
-            res.status(400).json(err)
+            res.status(400).json(err);
         });
 });
 
 router.post("/api/workouts", ({ body }, res) => {
     db.Workout.create({ body })
         .then(dbWorkout => {
-            res.json(dbWorkout)
+            res.json(dbWorkout);
         })
         .catch(err => {
-            res.status(400).json(err)
+            res.status(400).json(err);
         });
-})
+});
 
 router.put("/api/workouts/:id", (req, res) => {
-
-})
+    db.Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $inc: { totalDuration: req.body.duration },
+            $push: { exercises: req.body }
+        },
+        { new: true })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 router.get("/api/workouts/range", (req, res) => {
+    db.Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" },
+                combinedWeight: { $sum: "$exercises.weight" }
+            }
+        },
+        {
+            $sort: { day: -1 }
+        },
+        { $limit: 7 },
 
-})
+    ])
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
+
+
